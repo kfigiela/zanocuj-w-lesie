88074b38c08a62344af60e8cc94db573102cbd17
Kamil Figiela <kamil.figiela@gmail.com>
2020-01-21 10:36:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bushcraft: Mapa obszarów pilotażowych
+
+Mapa przygotowana w oparciu o dane pobrane z [Banku Danych o Lasach](https://www.bdl.lasy.gov.pl/).
---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+curl --request GET \
+  --url 'http://mapserver.bdl.lasy.gov.pl/ArcGIS/rest/services/Mapa_turystyczna/MapServer/51/query?where=objectId%3E0&f=json' \
+  -o data/51.json \
+  --header 'accept: text/html' \
+  --header 'accept-language: pl-pl' \
+  --header 'connection: keep-alive' \
+  --header 'host: mapserver.bdl.lasy.gov.pl' \
+  --header 'referer: https://www.bdl.lasy.gov.pl/portal/mapy' \
+  --header 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15' \
+  --compressed
+
+curl --request GET \
+  --url 'http://mapserver.bdl.lasy.gov.pl/ArcGIS/rest/services/Mapa_turystyczna/MapServer/51/query?where=objectId%3E0&f=kmz' \
+  -o data/51.kmz \
+  --header 'accept: text/html' \
+  --header 'accept-language: pl-pl' \
+  --header 'connection: keep-alive' \
+  --header 'host: mapserver.bdl.lasy.gov.pl' \
+  --header 'referer: https://www.bdl.lasy.gov.pl/portal/mapy' \
+  --header 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15' \
+  --compressed
+
+togpx data/51.json > data/51.gpx
